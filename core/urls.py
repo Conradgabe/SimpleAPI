6a84cf2a953fd4fb6289062230d5bb3ae6c57718
Conradgabe <gisuekebho5880@gmail.com>
2022-11-03 23:40:47 +0000

    added the new url path

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Sample, name='profile')
+    path('', views.Sample, name='profile'),
+    path('value', views.SamplePost, name='value')
 ]
