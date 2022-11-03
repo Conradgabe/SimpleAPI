de5249f3ba7c2a63368bfea86af6281932bbc774
Conradgabe <gisuekebho5880@gmail.com>
2022-11-03 23:40:23 +0000

    created a new operations serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 
-from .models import Profile
+from .models import Profile, Operations
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['slackUsername', 'backend', 'age', 'bio']
+
+class OperationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Operations
+        fields = '__all__'
